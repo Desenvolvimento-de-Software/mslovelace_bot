12381afecfd836a215da8ffebc0cd46d81f1a66c
Marcos Leandro <mleandrojr@gmail.com>
2021-12-09 01:43:49 -0200

    Added the SELECT statement query builder.

--- src/model/mysql/select.ts
+++ src/model/mysql/select.ts
@@ -41,37 +41,76 @@ export default class Select extends Builder {
     }
 
     /**
-     *
-     * @returns Returns the query fields.
+     * Parses and returns the query fields.
      *
      * @author Marcos Leandro
      * @since  1.0.0
+     *
+     * @returns {string}
      */
     private parseFields(): string {
         return this.fields.join(", ");
     }
 
+    /**
+     * Parses and returns the query conditions.
+     *
+     * @author Marcos Leandro
+     * @since  1.0.0
+     *
+     * @returns {string}
+     */
     private parseConditions(): string {
         return this.conditions.length ? this.conditions.join(" ") : "";
     }
 
+    /**
+     * Parses and returns the query grouping.
+     *
+     * @author Marcos Leandro
+     * @since  1.0.0
+     *
+     * @returns {string}
+     */
     private parseGroup(): string {
         return this.group.length ? this.group.join(", ") : "";
     }
 
+    /**
+     * Parses and returns the query ordenation.
+     *
+     * @author Marcos Leandro
+     * @since  1.0.0
+     *
+     * @returns {string}
+     */
     private parseOrder(): string {
-        return this.order.length ? this.order.join(", ") : "";
+
+        let order = [];
+        for (let [key, value] of Object.entries(this.order)) {
+            order.push(`${value.field} ${value.direction}`);
+        }
+
+        return order.length ? "order by " + order.join(", ") : "";
     }
 
+    /**
+     * Parses and returns the query offset and limit.
+     *
+     * @author Marcos Leandro
+     * @since  1.0.0
+     *
+     * @returns {string}
+     */
     private parseLimit(): string {
 
         let limit = [];
 
-        if (this.queryOffset) {
+        if (typeof this.queryOffset !== "undefined") {
             limit.push(this.queryOffset);
         }
 
-        if (this.queryLimit) {
+        if (typeof this.queryLimit !== "undefined") {
             limit.push(this.queryLimit);
         }
 
