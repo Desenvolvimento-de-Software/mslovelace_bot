be941a1810478bf0d62679300fc5628a2e4b3095
Marcos Leandro <mleandrojr@gmail.com>
2023-06-15 01:31:34 -0300

    Added the report by reply validations.

--- src/command/Report.ts
+++ src/command/Report.ts
@@ -11,7 +11,6 @@
 
 import Command from "./Command.js";
 import Context from "../library/telegram/context/Context.js";
-import CommandContext from "../library/telegram/context/Command.js";
 import ChatHelper from "../helper/Chat.js";
 import Lang from "../helper/Lang.js";
 import Text from "../helper/Text.js";
@@ -67,10 +66,40 @@ export default class Report extends Command {
 
         const replyToMessage = this.context.message.getReplyToMessage();
         if (typeof replyToMessage !== "undefined") {
-            this.context.message.getReplyToMessage()!.reply(reportMessage, options);
+            this.reportByReply(reportMessage, options);
             return;
         }
 
         this.context.message.reply(reportMessage, options);
+    }
+
+    /**
+     * Reports an user by reply.
+     *
+     * @author Marcos Leandro
+     * @since  2023-06-15
+     *
+     * @param {string} reportMessage
+     * @param {Record<string, any>} options
+     */
+    private async reportByReply(reportMessage: string, options: Record<string, any>): Promise<void> {
+
+        const contextMessage = this.context.message.getReplyToMessage();
+        if (!contextMessage) {
+            return;
+        }
+
+        const contextUser = contextMessage.getUser();
+        if (contextUser.getId() === parseInt(process.env.TELEGRAM_USER_ID!)) {
+            this.context.message.reply(Lang.get("selfReportMessage"));
+            return;
+        }
+
+        if (await contextUser.isAdmin()) {
+            this.context.message.reply(Lang.get("adminReportMessage"));
+            return;
+        }
+
+        contextMessage.reply(reportMessage, options);
     }
 }
