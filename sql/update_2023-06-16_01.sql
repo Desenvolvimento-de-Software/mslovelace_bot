c5e7d98837b8c8ec2b28704d8bf9bc635ec6f03b
Marcos Leandro <mleandrojr@yggdrasill.com.br>
2023-06-16 13:30:55 -0300

    Added the new messages relationship structure.

--- sql/update_2023-06-16_01.sql
+++ sql/update_2023-06-16_01.sql
@@ -1,2 +1,6 @@
 ALTER TABLE `messages` ADD `thread_id` INT(10) UNSIGNED NULL AFTER `chat_id`;
 ALTER TABLE `messages` ADD `entities` TEXT NULL AFTER `content`;
+ALTER TABLE `messages` ADD `thread_id` INT(10) UNSIGNED NULL AFTER `chat_id`;
+ALTER TABLE `messages` ADD `entities` TEXT NULL AFTER `content`;
+ALTER TABLE `messages` DROP FOREIGN KEY `fk_messages_reply_to`;
+ALTER TABLE `messages` ADD CONSTRAINT `fk_messages_reply_to` FOREIGN KEY (`reply_to`) REFERENCES `messages`(`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
