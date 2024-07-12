48ed931dc209db6ae25cdab764d43948c4ee4964
Marcos Leandro <mleandrojr@yggdrasill.com.br>
2024-07-12 14:34:13 +0000

    Fexed an error with payload reading.

--- src/controller/Controller.ts
+++ src/controller/Controller.ts
@@ -110,7 +110,7 @@ export default class Controller {
      */
     protected async handle(payload: Record<string, any>): Promise<void> {
 
-        if (!payload.ok) {
+        if (!payload.update_id) {
             Log.save("Invalid payload.\n" + JSON.stringify(payload), "", false, "error");
             return Promise.resolve();
         }
